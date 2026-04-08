c9ff8a78ae1cb6172066a0352b1bf2f81b3402fe
v.vasilev <v.vasilev@up2technology.com>
2026-04-08 10:38:51 +0300

    feat(theme): add F1 dark color palette

---
+++ app/src/main/java/com/vvasilev/f1race/presentation/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.vvasilev.f1race.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// App palette
+val DarkBackground = Color(0xFF0D1117)
+val DarkSurface = Color(0xFF161B22)
+val DarkSurfaceVariant = Color(0xFF21262D)
+val AccentRed = Color(0xFFE10600)
+val AccentWhite = Color(0xFFF1F1F1)
+val TextPrimary = Color(0xFFE6EDF3)
+val TextSecondary = Color(0xFF8B949E)
+val TrackSurface = Color(0xFF3A3A3A)
+val TrackEdge = Color(0xFFCCCCCC)
+val TrackCurb = Color(0xFFE10600)
+val StartFinishLine = Color(0xFFFFFFFF)
+
+// F1 Team colors (2024 season)
+val RedBullColor = Color(0xFF3671C6)
+val FerrariColor = Color(0xFFE8002D)
+val McLarenColor = Color(0xFFFF8000)
+val MercedesColor = Color(0xFF27F4D2)
+val AstonMartinColor = Color(0xFF229971)
+val AlpineColor = Color(0xFFFF87BC)
+val WilliamsColor = Color(0xFF64C4FF)
+val RBColor = Color(0xFF6692FF)
+val SauberColor = Color(0xFF52E252)
+val HaasColor = Color(0xFFB6BABD)
